166928e1cb14f4a9a04a1180985de5db88bc1a99
Scotty Simpson <portrayme@mac.com>
2021-08-01 16:18:24 -0700

    initial commit

---
+++ components/App.js
@@ -0,0 +1,5 @@
+/* -- global m -- */
+
+export default {
+	view: () => m('h1', 'Hello, herb!')
+}
---
+++ main.js
@@ -0,0 +1,7 @@
+import mithril from 'mithril'
+import App from './components/App.js'
+
+window.m = mithril
+
+
+m.mount(document.body, App)
